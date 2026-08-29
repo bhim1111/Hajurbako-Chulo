c2188403c4fb28735d794595ac913b9bcfa32ddf
Bhim Timsina <bhimtimsiana63@gmail.com>
2026-08-29 13:37:15 +0545

    Fix static URL and storage for Railway

--- hajurbako_chulo_project/settings.py
+++ hajurbako_chulo_project/settings.py
@@ -128,9 +128,12 @@ USE_TZ = True
 
 # --- Static & Media ---
 
-STATIC_URL = "static/"
+# --- Static & Media ---
+
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.WhiteNoiseStaticStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
