0c816b6d4cf4f722f7f50e02f52f7786ed24c575
Bhim Timsina <bhimtimsiana63@gmail.com>
2026-08-30 21:43:55 +0545

    Fix missing favicon

--- hajurbako_chulo_project/settings.py
+++ hajurbako_chulo_project/settings.py
@@ -95,7 +95,7 @@ else:
 # --- Production hardening ---
 
 if not DEBUG:
-    SECURE_BROWSER_XSS_FILTER = True
+   
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
